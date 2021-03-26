e72c6ee2d2b01ffd1ce5c7ee5aec4c6196fa67d0
Marc MNilà <milamartosmarc5@gmail.com>
2021-03-26 11:31:30 +0100

    working

--- setupTestbed.py
+++ setupTestbed.py
@@ -4,6 +4,7 @@ import subprocess
 import shlex
 import sys
 import time
+import os
 
 ip = raw_input('Introdueix l\'adreça IP del testbed: ')
 inst = "ssh {} 'sudo apt-get install git'".format('pi@' + ip) 
@@ -11,15 +12,16 @@ cmd = "ssh {} 'git clone https://github.com/marc-mila/testbedCodes.git'".format(
 cmd1 = "ssh {} '/home/pi/testbedCodes/setup.sh {}'".format('pi@' + ip, ip)
 cmd2 = "ssh {} 'python /home/pi/testbedCodes/start.py {}'".format('pi@' + ip, ip)
 
-
+FNULL = open(os.devnull, 'w')
+ 
 subprocess.Popen(shlex.split(inst), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(5)
 subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(5)
 subprocess.Popen(shlex.split(cmd1), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 time.sleep(5)
-subprocess.Popen(shlex.split(cmd2), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+subprocess.Popen(shlex.split(cmd2), stdout=FNULL)
 
 print "Testbed started!"
 
-sys.exit(0)
+#sys.exit(0)
